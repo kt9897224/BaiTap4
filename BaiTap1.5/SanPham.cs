4c74f3d57b7c85cf7a87e7163694ee9d675c2341
Admin <Admin@LAPTOP-8CVDJNS6>
2024-01-19 11:02:17 +0700

    Add project files.

---
+++ BaiTap1.5/SanPham.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap1._5
+{
+    internal class SanPham
+    {
+        
+        public int MaSanPham { get; set; }
+
+        public string TenSp { get; set; }
+
+        public int DonGia { get; set; }
+
+        public int SoLuong { get; set; }
+
+
+        public SanPham()
+        {
+                
+        }
+
+        public SanPham(int  maSanPham, string tenSp, int donGia, int soLuong)
+        {
+            MaSanPham = maSanPham;
+            TenSp = tenSp;
+            DonGia = donGia;
+            SoLuong = soLuong;
+        }
+
+    }
+}
